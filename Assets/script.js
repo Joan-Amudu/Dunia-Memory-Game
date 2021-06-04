78243755c9de03746744321cddadc7a4499c1332
joan <joan.baare@gmail.com>
2021-06-04 20:00:10 +0200

    All images to flags added to their respective div's and flip card function rendered

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+//Flipping cards
+const cards = document.querySelectorAll('.flip-card-inner');
+function flipCard() {
+    this.classList.toggle('flip');  
+}
+cards.forEach((card) => card.addEventListener('click', flipCard));
+
+// random 
+
+// start Game
+
+// match found
+
+// end game
+
+// display results
+
