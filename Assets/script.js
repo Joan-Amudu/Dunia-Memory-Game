60e05bd417a8c06c98c72044bf9836b5865d14d4
joan <joan.baare@gmail.com>
2021-06-05 20:57:37 +0200

    added flip sound to sounds folder and declared soundControl class and cardGame class

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,43 @@
-if(document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+
+class SoundControls{
+    constructor() {
+        this.flipSound = new Sound('assets/sounds/flipsound.wav');
+    }
+    flip() {
+        this.flipSound.play();
+    }
+}
+class cardGame {
+    constructor(cardArray) {
+        this.cards = cardArray
+        this.scores = document.getElementsByClassName('score');
+        this.timer = document.getElementsByClassName('time')
+    }
+    playGame() {
+        this.cardsToCheck = null;
+        this.matchCards = [];
+        this.busy = true;
+    }
+
+    flipCard(card){
+        if(this.cardFlipped(card)) {
+            this.sound.flipsound();
+        }
+
+    }
+
+    cardFlipped(card) {
+        return true;
+        //return !this.busy && !this.matchCards.includes(card) && card !== this.cardsToCheck;
+    }
 }
 
 function ready() {
     let cardArray = Array.from(document.getElementsByClassName('flip-card-inner'));
+    let game = new cardGame(cardArray);
     cardArray.forEach(card => {
         card.addEventListener('click',() => {
+            game.flipCard(card);
 
         })
     })
@@ -34,3 +64,12 @@ function ready() {
 // display results
 
 //cards.forEach((card) => card.addEventListener('click', flipCard));
+
+
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+
+let soundControls = new SoundControls();
