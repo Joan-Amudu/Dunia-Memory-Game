3d75b9ef38f5c8ed8dab11e96984a2c561d7e58c
joan <joan.baare@gmail.com>
2021-06-05 20:07:22 +0200

    Reduced cards to 12 pair - 24 cards totals, added Javascript ready function

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,36 @@
-//Flipping cards
-const cards = document.querySelectorAll('.flip-card-inner');
-function flipCard() {
-    this.classList.toggle('flip');  
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
 }
-cards.forEach((card) => card.addEventListener('click', flipCard));
+
+function ready() {
+    let cardArray = Array.from(document.getElementsByClassName('flip-card-inner'));
+    cardArray.forEach(card => {
+        card.addEventListener('click',() => {
+
+        })
+    })
+}
+
+
+
+//Flipping cards
+
+//const cards = document.querySelectorAll('.flip-card-inner');
+//function flipCard() {
+//   this.classList.toggle('flip');  
+//}
 
 // random 
 
 // start Game
 
-// match found
+// matching cards
+
 
 // end game
 
 // display results
 
+//cards.forEach((card) => card.addEventListener('click', flipCard));
